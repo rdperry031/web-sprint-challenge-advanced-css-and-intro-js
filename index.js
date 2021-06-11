74a9e429720ea0fa2732436560225aff5102fe30
rdperry031 <rdperry031@gmail.com>
2021-06-11 12:42:52 -0500

    copmleted taks in index.js except for task 4, which I was stuck on for quite a while before moving on for now

--- index.js
+++ index.js
@@ -242,8 +242,10 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  for(let i=0;i<array.length;i++){
+  }
+
 }
 
 
@@ -257,10 +259,12 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, i){
+  for(let i = 0; i < array.length; i++){
+      array.splice(i, 1);
+    return array.length;
 }
-   
+}   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -278,9 +282,21 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+function addArtist(array){
+    let newArtist = [
+      {
+      id: 20,
+      name: 'Richard Perry', 
+      years: '1989' - '2021',
+      genre: 'Web Design', 
+      nationality: 'American',
+      bio:   'lorem ipsum'
+    }
+  ]
+  array[20]=newArtist;
+return array  
+}
+
 
   
 
@@ -291,9 +307,16 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+    let  manyPaintings = [];
+  for(let i=0; i<array.length; i++){
+    if(array[i].paintings > 100){
+      manyPaintings.push(array[i].name)
+    }
 }
+return manyPaintings;  
+}
+
 
 
 
